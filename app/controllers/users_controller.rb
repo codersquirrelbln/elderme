46784b44304fa7bc32748875daf7afb7040f77c8
Monther Al Gbawi <m_algbawi@outlook.com>
2019-08-27 12:33:50 +0200

    usercontrollernew

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,15 @@
 class UsersController < ApplicationController
-   @user = User.new(user_params)
+  @user = User.new(user_params)
   if user.save
     redirect_to dashboard_users_path, notice: 'New user successfully created'
-   else
-  redirect_to :back
+  else
+    redirect_to :back
     flash[:info] = "Something is wrong, try again."
-   end
+  end
+
+  private
+
+  def users_params
+    params.require(:user).permit(:first_name, :last_name, :location, :city, :gender, :phone_number, :age, :mobility, :biography, :email)
+  end
 end
