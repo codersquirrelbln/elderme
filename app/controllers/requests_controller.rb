51d1cb428299c611f01602475c56f0ef8ddac267
Elisa <codersquirrelbln@gmail.com>
2019-08-28 14:00:08 +0200

    add request-new-view add request routes

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -1,5 +1,29 @@
 class RequestsController < ApplicationController
+   # skip_before_action :authenticate_user!, only: [:new, :create, :index, :show]
   def new
     @request = Request.new
+  end
+
+  def create
+    @request = Request.new(requests_params)
+    @user = User.find(params[:user_id])
+    if @request.save
+      redirect_to
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @request = Request.find(params[:id])
+    @user = User.find(params[:user_id])
+    @request.delete
+
+  end
+
+  private
+
+  def requests_params
+    params.require(:request).permit(:user_id, :name, :photo)
   end
 end
