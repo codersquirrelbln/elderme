95e7ddab220920961864657501d6c89b0865cc7b
Elisa <codersquirrelbln@gmail.com>
2019-08-31 13:02:39 +0200

    fixing time-input flatpickr

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,9 +1,6 @@
 import "bootstrap";
-<<<<<<< HEAD
-import "../plugins/flatpickr";
-=======
 import initializeFlatpickr from "../plugins/flatpickr";
 import "flatpickr/dist/flatpickr.min.css" // Note this is important!
 
 initializeFlatpickr();
->>>>>>> master
+
--- app/javascript/plugins/flatpickr.js
+++ app/javascript/plugins/flatpickr.js
@@ -1,15 +1,11 @@
-// import '../plugins/flatpickr';
-// import flatpickr from 'flatpickr'
-// import 'flatpickr/dist/flatpickr.min.css';
-
-// flatpickr('.datepicker', {});
-
-
 import flatpickr from "flatpickr"
+import "flatpickr/dist/flatpickr.min.css"
 
 function initializeFlatpickr() {
   flatpickr(".datepicker", {
     minDate: "today",
+    altInput: true,
+    altFormat: "F j, Y",
     dateFormat: "Y-m-d",
     disable: [
         function(date) {
@@ -17,8 +13,88 @@ function initializeFlatpickr() {
             return !(date.getDate() % 8);
         }
     ]
-  // mode: "range"
+  });
+  flatpickr(".timepicker", {
+    enableTime: true,
+    noCalendar: true,
+    dateFormat: "H:i",
   });
 }
 
 export default initializeFlatpickr;
+
+
+// confirm
+// {
+//     "enableTime": true,
+//     "plugins": [new confirmDatePlugin({})]
+// }
+
+
+// user friendly datepicker
+// {
+//     altInput: true,
+//     altFormat: "F j, Y",
+//     dateFormat: "Y-m-d",
+// }
+
+
+
+// to disable certain dates
+// {
+//     disable: ["2025-01-30", "2025-02-21", "2025-03-08", new Date(2025, 4, 9) ],
+//     dateFormat: "Y-m-d",
+// }
+
+// enabling range(s) of dates
+// {
+//     enable: [
+//         {
+//             from: "2025-04-01",
+//             to: "2025-05-01"
+//         },
+//         {
+//             from: "2025-09-01",
+//             to: "2025-12-01"
+//         }
+//     ]
+// }
+
+
+// time pickr with limits
+// {
+//     enableTime: true,
+//     noCalendar: true,
+//     dateFormat: "H:i",
+//     minDate: "16:00",
+//     maxDate: "22:30",
+// }
+
+
+// time pickr
+// {
+//     enableTime: true,
+//     noCalendar: true,
+//     dateFormat: "H:i",
+// }
+
+// date and time
+// {
+//     enableTime: true,
+//     dateFormat: "Y-m-d H:i",
+// }
+
+
+// flatpickr(".datepicker", {
+//     minDate: "today",
+//     dateFormat: "Y-m-d",
+//     disable: [
+//         function(date) {
+//             // disable every multiple of 8
+//             return !(date.getDate() % 8);
+//         }
+//     ]
+//   // mode: "range"
+//   });
+// }
+
