acd685ee07ecb996be6a7e78d771073881ff9b11
Elisa <codersquirrelbln@gmail.com>
2019-09-02 18:04:25 +0200

    add sweet-alert to request-sent and request edited

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,5 +1,30 @@
 import "bootstrap";
 import initializeFlatpickr from "../plugins/flatpickr";
 import "flatpickr/dist/flatpickr.min.css" // Note this is important!
+import { initSweetalert } from '../plugins/init_sweetalert';
 
 initializeFlatpickr();
+
+initSweetalert('#sweet-alert', {
+  title: "Wonderful!",
+  html: `Your message has been sent <a href = "/requests"><button class = "btn btn-primary"> OK </button></a>`,
+  icon: "success",
+  showConfirmButton: false,
+  }, (value) => {
+  if (value) {
+    const link = document.querySelector('.alert');
+    link.click();
+  }
+});
+
+initSweetalert('#sweet-alert-edit', {
+  title: "Wonderful!",
+  html: `Your request has been updated <a href = "/requests"><button class = "btn btn-primary"> OK </button></a>`,
+  icon: "success",
+  showConfirmButton: false,
+  }, (value) => {
+  if (value) {
+    const link = document.querySelector('.alert');
+    link.click();
+  }
+});
