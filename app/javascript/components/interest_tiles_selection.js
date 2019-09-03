2bee0b3860fab093c9a5d78bde911421fbb5c690
Ronan Recune <r.recune@gmail.com>
2019-09-03 11:55:27 +0200

    filters

--- app/javascript/components/interest_tiles_selection.js
+++ app/javascript/components/interest_tiles_selection.js
@@ -1,8 +1,16 @@
 
 function checked(i){
+
   this.classList.toggle("active");
+  const label = this.parentElement
+  if (this.classList.contains("active")) {
+
+  } else {
+
+  }
   console.log({name:i.target.name, value: i.target.value});
 }
 
 const filters = document.querySelectorAll('.check-me');
 filters.forEach(filter => filter.addEventListener('click', checked))
+
