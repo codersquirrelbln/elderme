0401af4245e32a7dd2328bd7339697f76c6175f9
Ronan Recune <r.recune@gmail.com>
2019-09-03 12:49:54 +0200

    filters done

--- app/javascript/components/interest_tiles_selection.js
+++ app/javascript/components/interest_tiles_selection.js
@@ -3,10 +3,13 @@ function checked(i){
 
   this.classList.toggle("active");
   const label = this.parentElement
+  console.log(label)
   if (this.classList.contains("active")) {
-
+    label.classList.remove("circle-activities")
+    label.classList.add("circle-activities-selected")
   } else {
-
+    label.classList.remove("circle-activities-selected")
+    label.classList.add("circle-activities")
   }
   console.log({name:i.target.name, value: i.target.value});
 }
